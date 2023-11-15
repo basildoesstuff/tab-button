20081fe12c1665a48bc586dd987f8b3a271bd5cb
basildoesstuff <contact.basilsunny@gmail.com>
2023-11-15 11:33:05 +0530

    The button design pattern

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import TabButton from "./TabButton";
 import "./styles.css";
 
 export default function App() {
+     function handleSelect(){
+     console.log("hellow world --selected")
+    } 
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+     
+    <section>
+        <menu>
+        <TabButton onSelect={handleSelect} >basil</TabButton>
+        <TabButton onSelect={handleSelect}>fun</TabButton>
+        <TabButton onSelect={handleSelect}>alukkail</TabButton>
+        </menu>
+        Dynamic Content
+    </section>
+    
     </div>
   );
 }
---
+++ src/TabButton.jsx
@@ -0,0 +1,13 @@
+export default function TabButton ({children,onSelect}){
+
+    function handleClick(){
+        console.log("Hello World");
+
+    }
+    return(
+        <li>
+               <button onClick={onSelect}>{children}</button>
+        </li>
+    )
+
+}
